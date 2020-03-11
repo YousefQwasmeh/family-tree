349268b27f244ed221fde5bd5ab181b9ed403810
YousefQwasmeh <151037@ppu.edu.ps>
2020-03-11 16:41:29 +0200

    get tree

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const getChildren = require("./getChildren");
 const getParents = require("./getParents");
+const getTree = require("./getTree");
 router.get("/", (req, res) => {
   res.send("00000");
 });
 router.get("/get-parents", getParents);
 router.get("/get-children", getChildren);
+router.get("/get-tree", getTree);
 module.exports = router;
