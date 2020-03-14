b5407bfa6d5474a608f1e9b116ab24e6fb385a07
YousefQwasmeh <151037@ppu.edu.ps>
2020-03-15 01:59:31 +0200

    add event listener to names for shurtcut tree add level up button to larger the tree

--- client/src/components/tree.js
+++ client/src/components/tree.js
@@ -4,24 +4,25 @@ import axios from "axios";
 class Tree extends React.Component {
   state = {
     tree: {},
-    person: { id: 17, name: "loading..." },
+    person: { id: 0, name: "loading..." },
     maxWidth: 0
   };
   componentDidMount() {
     this.getPerson(17);
-    axios(`/api/tree/${this.state.person.id}`).then(({ data }) => {
+    this.getTree(17);
+  }
+  getTree = rootId => {
+    axios(`/api/tree/${rootId}`).then(({ data }) => {
       this.setState({ tree: data }, () => {
-        setTimeout(() => {
-          const big = document.getElementsByClassName(`${this.state.person.id}`)
-            ? document.getElementsByClassName(`${this.state.person.id}`)[0]
-            : null;
-          this.setState({
-            maxWidth: big && big.clientWidth ? big.clientWidth : 0
-          });
-        }, 100);
+        const big = document.getElementsByClassName(`${rootId}`)
+          ? document.getElementsByClassName(`${rootId}`)[0]
+          : null;
+        this.setState({
+          maxWidth: big && big.clientWidth ? big.clientWidth : 0
+        });
       });
     });
-  }
+  };
   getPerson = personId => {
     axios(`/api/person/${personId}`)
       .then(({ data }) => this.setState({ person: data[0] }))
@@ -29,15 +30,49 @@ class Tree extends React.Component {
         console.log(err);
       });
   };
-  getTree = rootId => {
+  getParent = childId => {
+    axios(`/api/parent/${childId}`)
+      .then(({ data }) => {
+        if (data[0])
+          this.setState({ person: data[0] }, () => {
+            const big = document.getElementsByClassName(
+              `${this.state.person.id}`
+            )
+              ? document.getElementsByClassName(`${this.state.person.id}`)[0]
+              : null;
+            this.setState({
+              maxWidth: big && big.clientWidth ? big.clientWidth : 0
+            });
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  drawTree = rootId => {
     return (
       <div className={"div " + rootId}>
         {this.state.tree[rootId]
           ? this.state.tree[rootId].map(person => {
               return (
                 <section>
-                  <h5>{person.name}</h5>
-                  {this.getTree(person.id)}
+                  <h5
+                    onClick={() => {
+                      this.setState({ person }, () => {
+                        const big = document.getElementsByClassName(
+                          `${person.id}`
+                        )
+                          ? document.getElementsByClassName(`${person.id}`)[0]
+                          : null;
+                        this.setState({
+                          maxWidth: big && big.clientWidth ? big.clientWidth : 0
+                        });
+                      });
+                    }}
+                  >
+                    {person.name}
+                  </h5>
+                  {this.drawTree(person.id)}
                 </section>
               );
             })
@@ -49,10 +84,18 @@ class Tree extends React.Component {
     return (
       <>
         {/* <input type='text' /> */}
+        <h5
+          className='level-up'
+          onClick={() => {
+            this.getParent(this.state.person.id);
+          }}
+        >
+          lєvєl up⇪
+        </h5>
         <h5 style={{ width: this.state.maxWidth || "auto" }}>
           {this.state.person.name}
         </h5>
-        {this.getTree(this.state.person.id)}
+        {this.drawTree(this.state.person.id)}
       </>
     );
   }
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,13 +2,15 @@ const express = require("express");
 const router = express.Router();
 const getChildren = require("./getChildren");
 const getParents = require("./getParents");
+const getParent = require("./getParent");
 const getTree = require("./getTree");
 const getPerson = require("./getPerson");
 router.get("/", (req, res) => {
   res.send("00000");
 });
-router.get("/parents/childId", getParents);
-router.get("/children/parentId", getChildren);
+router.get("/parents/:childId", getParents);
+router.get("/parent/:childId", getParent);
+router.get("/children/:parentId", getChildren);
 router.get("/tree/:rootId", getTree);
 router.get("/person/:personId", getPerson);
 module.exports = router;
